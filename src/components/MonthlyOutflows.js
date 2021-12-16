50e5b65b928ada31b1259ec27e891e0964d5802e
Manaz PR <manas0096@gmail.com>
2021-12-17 00:13:49 +0530

    commit

--- src/components/MonthlyOutflows.js
+++ src/components/MonthlyOutflows.js
@@ -1,11 +1,15 @@
+import "./StreamList.css";
+import Card from "react-bootstrap/esm/Card";
+
 function MonthlyOutflows(props) {
     return (
         <div>
             <h3><strong>Total Monthly Outflows</strong></h3>
-            <h3>{`$${props.outflows.toFixed(2)}`}</h3>
+            <Card className="outflowNumber"> 
+            <h3>{`$${props.outflows.toFixed(2)}`}</h3></Card>
             
            <br></br> <h5><strong>Your balance will run out on:</strong></h5>
-            <div classname= "date"><h6>{props.endDate}</h6></div>
+               <div classname= "date"><h6>{props.endDate}</h6></div>
           
             </div>
         )
