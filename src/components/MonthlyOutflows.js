533dfa2f70a8c15b3e61325fe3a566885717715f
Manaz PR <manas0096@gmail.com>
2021-12-16 23:24:03 +0530

    commit

--- src/components/MonthlyOutflows.js
+++ src/components/MonthlyOutflows.js
@@ -1,10 +1,10 @@
 function MonthlyOutflows(props) {
     return (
         <div>
-            <h2><strong>Total Monthly Outflows</strong></h2>
+            <h3><strong>Total Monthly Outflows</strong></h3>
             <h3>{`$${props.outflows.toFixed(2)}`}</h3>
             
-            <h5><strong>Your balance will run out on:</strong></h5>
+           <br></br> <h5><strong>Your balance will run out on:</strong></h5>
             <div classname= "date"><h6>{props.endDate}</h6></div>
           
             </div>
